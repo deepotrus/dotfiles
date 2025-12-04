9777fa57ce0eb030445c97d3263e74b39da44ae9
andrei <>
2025-12-04 11:59:32 +0100

    added tmux clipboard for vim past through tmux ssh

--- nvim/.config/nvim/init.lua
+++
@@ -1,8 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
-vim.cmd("inoremap jk <Esc>")
-vim.cmd("inoremap kj <Esc>")
-vim.cmd("LushwalCompile")
-require("transparent").clear_prefix("NeoTree")
-require("transparent").clear_prefix("lualine")
